bd5ab09b01adbbf6a7f684c622423be08389335b
Christopher Grim <christopher.grim@gmail.com>
2007-03-13 00:18:55 +0000

    ratpoisonrc: Exec xmodmap and xsetroot on startup.
    startup.py: Add new functions to clear readline history and setup a
                color prompt.

--- startup.py
+++ startup.py
@@ -1,5 +1,7 @@
 from pprint import pprint as pp
 from pdb import pm
+import os
+import sys
 
 def has_readline():
     try:
@@ -7,6 +9,7 @@ def has_readline():
         return True
     except ImportError:
         return False
+
     
 def setup_history():
     """Setup a persisitent history file."""
@@ -20,14 +23,39 @@ def setup_history():
     import atexit
     atexit.register(readline.write_history_file, histfile)
 
+    
 def setup_completer():
     """Setup readline completetion."""
     import readline
     import rlcompleter
     readline.parse_and_bind("tab: complete")
-    
+
+
+def setup_prompt():
+    import os
+    import sys
+    BLUE = "\033[1;34m"
+    NOCOLOR = "\033[0m"
+    if os.environ['TERM'] != 'dumb':
+        sys.ps1 = "%s>>> %s" % (BLUE, NOCOLOR)
+
+        
 if has_readline():
     setup_history()
     setup_completer()
 
-del(has_readline, setup_history, setup_completer)
+    def hcl():
+        """Clear readline history."""
+        try:
+            clear = raw_input("Clear history? [y|N]: ")
+            if clear in ('y', 'Y', 'yes', 'Yes'):
+                import readline
+                readline.clear_history()
+
+        except EOFError:
+            pass
+
+           
+setup_prompt()
+        
+del(has_readline, setup_history, setup_completer, setup_prompt)
